8f50ef1391a282e8761ac00d93f6d7e958cd48fd
Ceora Ford <ceoraallthetime@gmail.com>
2018-07-24 18:54:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@ puts "Enter a value"
 value = gets.chomp.to_i 
 
 if value %15==0 
-  puts "Fizz"
+  puts "Fizzbuzz"
 elsif value % 5 ==0 
   puts "Buzz"
 elsif value %3 ==0 
-  puts "Fizzbuzz"
+  puts "Fizz"
 else 
 end 
